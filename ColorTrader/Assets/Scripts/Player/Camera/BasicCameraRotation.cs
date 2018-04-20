e585c97bf2e84a13ce368e31fcd29a6f185b37ed
BuildTools <unconfigured@null.spigotmc.org>
2018-04-20 22:12:00 +0200

    Basic First Person Camera + Movement

---
+++ ColorTrader/Assets/Scripts/Player/Camera/BasicCameraRotation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BasicCameraRotation : MonoBehaviour {
+
+    public enum RotationAxis
+    {
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    [SerializeField] private RotationAxis _axis = RotationAxis.MouseX;
+
+    private float _rotationMin = -45f;
+    private float _rotationMax = 45f;
+
+    [SerializeField] private float _sensivity = 10f;
+
+    private float _rotationX = 0;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        switch (_axis)
+        {
+            case RotationAxis.MouseX:
+                transform.Rotate(0, Input.GetAxis("Mouse X") * _sensivity, 0);
+                break;
+            case RotationAxis.MouseY:
+                _rotationX -= Input.GetAxis("Mouse Y") * _sensivity;
+                _rotationX = Mathf.Clamp(_rotationX, _rotationMin, _rotationMax);
+
+                float rotationY = transform.localEulerAngles.y;
+
+                transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+                break;
+        }
+    }
+}
---
+++ ColorTrader/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private float _speed;
+    [SerializeField] private float _gravity;
+
+    private CharacterController _characterController;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * _speed;
+        float deltaZ = Input.GetAxis("Vertical") * _speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, _speed);
+        movement.y = _gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _characterController.Move(movement);
+    }
+}
